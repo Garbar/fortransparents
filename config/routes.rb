932e06066658051689be57af8d8ab80455322b25
Garbar <lorelgarbar@yahoo.com>
2017-12-08 10:32:16 +0200

    Add model Article, add seeds for 50 articles
    add nav partial, add friendly_id, add high_voltage

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :articles, only: [:index, :show]
   namespace :admin do
     resources :users
 
     root to: "users#index"
   end
+  get 'pages/about' => 'high_voltage/pages#show', id: 'about'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
