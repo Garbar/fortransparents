7b18775ddc4f0ea97da82f54afac5b9225bf8030
Garbar <lorelgarbar@yahoo.com>
2017-12-09 12:35:55 +0200

    Add name, avatar and role to user
    create login and register page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: [:user, :blogger, :admin]
 end
