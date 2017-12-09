7cae09784b4c5a6c62e04f6c415c3b13d1afbb5d
Garbar <lorelgarbar@yahoo.com>
2017-12-09 16:12:19 +0200

    Add settings for administrate
    add password field, Field::Carrierwave

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:index, :show]
+  mount Ckeditor::Engine => '/ckeditor'
   namespace :admin do
     resources :users
+    resources :articles
 
     root to: "users#index"
   end
+
+  resources :articles, only: [:index, :show]
   get 'pages/about' => 'high_voltage/pages#show', id: 'about'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'dashboard#index'
+  root to: 'home#index'
 end
